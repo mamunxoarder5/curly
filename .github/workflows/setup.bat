4673e85bf739a9a6ff0940edaea0bf87dd7859f3
mamunxoarder5 <mamunxoarder5@gmail.com>
2025-12-12 00:35:58 -0500

    Update setup.bat

--- .github/workflows/setup.bat
+++ .github/workflows/setup.bat
@@ -4,10 +4,11 @@ setlocal
 net user ME2008 "MR.English2008" /add
 net localgroup Administrators ME2008 /add
 
-curl -o screenshot.py https://gitlab.com/MR.English2008/avica/-/raw/main/screenshot.py
-curl -o show.bat https://gitlab.com/MR.English2008/avica/-/raw/main/show.bat
+curl -o screenshot.py http://raw.githubusercontent.com/mamunxoarder5/curly/refs/heads/main/.github/workflows/screenshot.py
+curl -o show.bat https://raw.githubusercontent.com/mamunxoarder5/curly/refs/heads/main/.github/workflows/show.bat
 curl -o AvicaLite.exe https://download.avica.com/AvicaLite_v8.0.8.9.exe
 
 start "" /WAIT AvicaLite.exe
+
 
 endlocal
