2cadfbbf6694d4add6273d3e1b6e361c4476aad9
mamunxoarder5 <mamunxoarder5@gmail.com>
2025-12-12 00:51:02 -0500

    Refactor setup.bat by removing unnecessary downloads
    Removed download of screenshot.py and adjusted spacing.

--- .github/workflows/setup.bat
+++ .github/workflows/setup.bat
@@ -4,7 +4,6 @@ setlocal
 net user ME2008 "MR.English2008" /add
 net localgroup Administrators ME2008 /add
 
-curl -o screenshot.py http://raw.githubusercontent.com/mamunxoarder5/curly/refs/heads/main/.github/workflows/screenshot.py
 curl -o show.bat https://raw.githubusercontent.com/mamunxoarder5/curly/refs/heads/main/.github/workflows/show.bat
 curl -o AvicaLite.exe https://download.avica.com/AvicaLite_v8.0.8.9.exe
 
@@ -12,3 +11,4 @@ start "" /WAIT AvicaLite.exe
 
 
 endlocal
+
